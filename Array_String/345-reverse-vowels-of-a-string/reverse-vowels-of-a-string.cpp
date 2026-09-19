df99837eb8d3d73f8fc646a9182eb69ae5d8b808
Christian de Guzman <128949102+ccdeguzman@users.noreply.github.com>
2026-09-18 20:33:48 -0400

    Time: 2 ms (38.96%) | Memory: 10.2 MB (92.03%) - LeetSync

---
+++ Array_String/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+        bool isVowel(char c) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                return true;
+            }
+            return false;
+        }
+
+    string reverseVowels(string s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            while(start < end && !isVowel(s[start])) {
+                start++;
+            }
+            while(start < end && !isVowel(s[end])) {
+                end--;
+            }
+
+            swap(s[start], s[end]);
+
+            start++;
+            end--;
+        }
+        return s;
+    }
+    // Christian de Guzman
+};
